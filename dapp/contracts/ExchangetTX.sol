f22f73a92145305e061dee29ba07d8c27d129514
Sean Pollock <seanpollock3344@gmail.com>
2016-11-18 15:07:27 -0600

    initializing submitBid submitAsk functions-- these will be the public entry points to the Exchange Hub

--- dapp/contracts/ExchangetTX.sol
+++ dapp/contracts/ExchangetTX.sol
@@ -13,20 +13,20 @@ contract ExchangeTX {
     Bid[] public BidLedger;
     Ask[] public AskLedger;
 
-    // function submitBid(uint _price, uint _amount) returns (bool) {
-    //     Bid memory b;
-    //     b.price = _price;
-    //     b.amount = _amount;
-    //     BidLedger.push(b);
-    //     return true;
-    // }
+    function submitBid(uint _price, uint _amount) returns (bool) {
+        Bid memory b;
+        b.price = _price;
+        b.amount = _amount;
+        BidLedger.push(b);
+        return true;
+    }
 
-    // function submitAsk(uint_price, uint _amount) returns (bool) {
-    //     Ask memory a;
-    //     a.price = _price;
-    //     a.amount = _amount;
-    //     AskLedger.push(a);
-    //     return true;
+    function submitAsk(uint _price, uint _amount) returns (bool) {
+        Ask memory a;
+        a.price = _price;
+        a.amount = _amount;
+        AskLedger.push(a);
+        return true;
     }
 
 }
