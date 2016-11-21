47b0bef43b4c779fbd4da7ff6d569522de064371
Sean Pollock <seanpollock3344@gmail.com>
2016-11-21 10:51:25 -0600

    cleanAsk Ledger Function that truncates array length to remove fully matched orders

--- dapp/contracts/ExchangeTX.sol
+++ dapp/contracts/ExchangeTX.sol
@@ -1,124 +1,100 @@
+pragma solidity ^0.4.0;
+
 contract ExchangeTX {
-
     struct Bid {
-        address owner;
         uint price;
         uint amount;
-        uint date;
     }
-
     struct Ask {
-        address owner;
         uint price;
         uint amount;
-        uint date;
     }
 
-    Bid[] public Bids;
-    Ask[] public Asks;
+    Bid[] public BidLedger;
+    Ask[] public AskLedger;
 
-    modifier bidInMarket(uint _price) {
-        if (Asks.length > 0) {
-            if (_price < Asks[Asks.length -1].price) throw;
+    modifier bidInMarket(uint _price) { // Modifier
+        if (AskLedger.length > 0) {
+            if (_price < AskLedger[AskLedger.length-1].price ) throw;
         }
         _;
     }
 
     modifier askInMarket(uint _price) {
-        if (Bids.length > 0) {
-            if (_price > Bids[Bids.length-1].price) throw;
+        if (BidLedger.length > 0) {
+            if (_price > BidLedger[BidLedger.length-1].price) throw;
         }
         _;
     }
 
-    function submitBid(uint _price, uint _amount) bidInMarket(_price) external returns (bool) {
+    function submitBid(uint _price, uint _amount) bidInMarket(_price) returns (bool) {
         Bid memory b;
-        b.owner = msg.sender;
         b.price = _price;
         b.amount = _amount;
-        b.date = now;
-        for(uint i = 0; i < Bids.length; i++) {
-            if(Bids[i].price > _price) {
-                Bid[] memory tempBids = new Bid[](Bids.length - i);
-                for(uint j = i; j < Bids.length; j++) {
-                    tempBids[j-i] = Bids[j];
+        for(uint i = 0; i < BidLedger.length; i++) {
+            if (BidLedger[i].price > _price) {
+                Bid[] memory tempLedger = new Bid[](BidLedger.length - i);
+                for(uint j = 0; j < tempLedger.length; j++) {
+                    tempLedger[j] = BidLedger[j+i];
                 }
-                Bids[i] = b;
-                Bids.length = Bids.length + 1;
-                for(uint k = 0; k < tempBids.length; k++) {
-                    Bids[i+k+1] = tempBids[k];
-                }
-                if(Asks.length > 0) {
-                    matchBid(i, Asks.length-1);
+                BidLedger[i] = b;
+                BidLedger.length ++;
+                for(uint k = 0; k < tempLedger.length; k++) {
+                    BidLedger[k+i+1] = tempLedger[k];
                 }
                 return true;
             }
         }
-        Bids.push(b);
-        if(Asks.length > 0) {
-            matchBid(Bids.length-1, Asks.length-1);
-        }
+        BidLedger.push(b);
         return true;
     }
 
-    function submitAsk(uint _price, uint _amount) askInMarket(_price) external returns (bool) {
+    function submitAsk(uint _price, uint _amount) askInMarket(_price) returns (bool) {
         Ask memory a;
-        a.owner = msg.sender;
         a.price = _price;
         a.amount = _amount;
-        a.date = now;
-        for (uint i = 0; i < Asks.length; i ++) {
-            if(Asks[i].price < _price) {
-                Ask[] memory tempAsks = new Ask[](Asks.length - i);
-                for (uint j = i; j < Asks.length; j++) {
-                    tempAsks[j-i] = Asks[j];
+        for(uint i = 0; i < AskLedger.length; i++) {
+            if(AskLedger[i].price < _price) {
+                Ask[] memory tempLedger = new Ask[](AskLedger.length - i);
+                for(uint j = 0; j < tempLedger.length; j++) {
+                    tempLedger[j] = AskLedger[j+i];
                 }
-                Asks[i] = a;
-                Asks.length = Asks.length + 1;
-                for (uint k = 0; k < tempAsks.length; k++) {
-                    Asks[i+k+1] = tempAsks[k];
-                }
-                if(Bids.length > 0) {
-                    matchAsk(i, Bids.length-1);
+                AskLedger[i] = a;
+                AskLedger.length += 1;
+                for(uint k = 0; k < tempLedger.length; k++) {
+                    AskLedger[k+i+1] = tempLedger[k];
                 }
                 return true;
             }
         }
-        Asks.push(a);
-        if(Bids.length > 0) {
-            matchAsk(Asks.length-1, Bids.length-1);
-        }
+        AskLedger.push(a);
         return true;
     }
 
     function matchBid(uint bid_index, uint ask_index) returns (bool) {
-        if (Bids[bid_index].amount <= 0 || Bids[bid_index].price < Asks[ask_index].price) {
-            cleanAskLedger();
+        if(BidLedger[bid_index].amount <= 0 || BidLedger[bid_index].price < AskLedger[ask_index].price) {
             return true;
         }
-        Asks[ask_index].amount = Asks[ask_index].amount - 1;
-        Bids[bid_index].amount = Bids[bid_index].amount - 1;
-        if (Asks[ask_index].amount == 0) {
-            if (ask_index == 0) {
-                cleanAskLedger();
+        BidLedger[bid_index].amount--;
+        AskLedger[ask_index].amount--;
+        if(AskLedger[ask_index].amount <= 0 ) {
+            if(ask_index <= 0) {
                 return true;
             }
-            ask_index = ask_index - 1;
-            return(matchBid(bid_index, ask_index));
+            ask_index--;
+            return matchBid(bid_index, ask_index);
         }
-        return(matchBid(bid_index, ask_index));
+        return matchBid(bid_index, ask_index);
     }
 
     function matchAsk(uint ask_index, uint bid_index) returns (bool) {
-        if(Asks[ask_index].amount <= 0 || Asks[ask_index].price > Bids[bid_index].price) {
-            cleanBidLedger();
+        if(AskLedger[ask_index].amount <= 0 || AskLedger[ask_index].price > BidLedger[bid_index].price) {
             return true;
         }
-        Bids[bid_index].amount--;
-        Asks[ask_index].amount--;
-        if(Bids[bid_index].amount == 0) {
-            if(bid_index == 0) {
-                cleanBidLedger();
+        AskLedger[ask_index].amount--;
+        BidLedger[bid_index].amount--;
+        if(BidLedger[bid_index].amount <= 0) {
+            if (bid_index <= 0) {
                 return true;
             }
             bid_index--;
@@ -127,23 +103,13 @@ contract ExchangeTX {
         return(matchAsk(ask_index, bid_index));
     }
 
-    function cleanAskLedger() returns (bool) {
-        for(uint i = Asks.length - 1; i >= 0; i--) {
-            if (Asks[i].amount > 0) {
-                break;
+    function cleanAskLedger() returns (bool){
+        for(uint i = AskLedger.length - 1; i >= 0; i--) {
+            if(AskLedger[i].amount > 0) {
+                AskLedger.length = i + 1;
+                return true;
             }
         }
-        Asks.length = i + 1;
-        return true;
-    }
-
-    function cleanBidLedger() returns (bool) {
-        for(uint i = Bids.length -1; i >= 0; i--) {
-            if(Bids[i].amount > 0) {
-                break;
-            }
-        }
-        Bids.length = i + 1;
-        return true;
-    }
-} 
+        return false;
+    }  
+}
